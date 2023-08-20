c6714c2f17850748986fe4d3cba698dc5de33ab4
wannazid <77951232+wannazid@users.noreply.github.com>
2023-08-20 20:29:12 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name='nikreader',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(where='./'),
     description='Sebuah modul untuk membaca kode NIK pada KTP dan menjadi beberapa bagian.',
     url='https://github.com/wannazid/NIK-Reader',
     author='wannazid',
     author_email='malastech.id@gmail.com',
     license='MIT',
     data_files=[('nikreader', ['nikreader/database.json'])],
-    input_package_data=True,
+    include_package_data=True,
+    package_dir={'nikreader': 'nikreader'},
     keywords=['nikreader', 'NIK Ekstraktor', 'Python'],
 )
